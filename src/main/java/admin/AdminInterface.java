0f19474e27114c933d91260d7452e1b46f98d146
Virender Singh <virendersingh@Virenders-MacBook-Air.local>
2021-08-03 16:52:14 -0400

    docstring updated

--- src/main/java/admin/AdminInterface.java
+++ src/main/java/admin/AdminInterface.java
@@ -5,7 +5,7 @@ public interface AdminInterface extends java.rmi.Remote
     /**
      * Method to register first time users to the service
      * @param entity containing the info about the new user
-     * @return BrokerInfoPayload informing the user about one of the active broker
+     * @return BrokerInfoPayload informing the user about one of the active brokers
      * @throws InterruptedException
      */
     BrokerInfoPayload registerNewUser(AbstractNetworkEntity entity) throws java.rmi.RemoteException, InterruptedException;
@@ -14,7 +14,7 @@ public interface AdminInterface extends java.rmi.Remote
      * Method to register first time brokers to the service
      * @param serverIp
      * @param portNumber
-     * @return boolean check informing the broker if it has been successfully registered or notifies of error if any
+     * @return boolean check informing the broker if it has been successfully registered
      */
     boolean registerNewBroker(String serverIp, int portNumber) throws java.rmi.RemoteException, InterruptedException;
 
