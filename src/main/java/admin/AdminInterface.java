5515d49bad3a82d769c5be2b20e2fcdeae27e990
Virender Singh <virendersingh@Virenders-MacBook-Air.local>
2021-08-04 20:31:52 -0400

    BrokerInfoPayload and UserInfoPayload passed as arguments

--- src/main/java/admin/AdminInterface.java
+++ src/main/java/admin/AdminInterface.java
@@ -4,18 +4,17 @@ public interface AdminInterface extends java.rmi.Remote
 {
     /**
      * Method to register first time users to the service
-     * @param entity containing the info about the new user
+     * @param UserInfoPayload containing the info about the new user
      * @return BrokerInfoPayload informing the user about one of the active brokers
      * @throws InterruptedException
      */
-    BrokerInfoPayload registerNewUser(AbstractNetworkEntity entity) throws java.rmi.RemoteException, InterruptedException;
+    BrokerInfoPayload registerNewUser(UserInfoPayload user) throws java.rmi.RemoteException, InterruptedException;
 
     /**
      * Method to register first time brokers to the service
-     * @param serverIp
-     * @param portNumber
+     * @param BrokerInfoPayload containing the info about the new broker
      * @return boolean check informing the broker if it has been successfully registered
      */
-    boolean registerNewBroker(String serverIp, int portNumber) throws java.rmi.RemoteException, InterruptedException;
+    boolean registerNewBroker(BrokerInfoPayload broker) throws java.rmi.RemoteException, InterruptedException;
 
 }
