aac789c33cfff5c4b66cd2dedc61118528c4b203
kulkarniakash <kulkarni.akash@gmail.com>
2021-08-07 17:17:54 -0400

    deleted some interfaces

--- src/main/java/client/ClientToBrokerInterface.java
+++ src/main/java/client/ClientToBrokerInterface.java
@@ -2,19 +2,19 @@ package client;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.List;
 
 /**
  * Interface used by Broker to call Client methods
  * @param <T> interfaces for Message
  * @param <V> interfaces for Response
  */
-public interface ClientToBrokerInterface<T, V> extends ClientToPeerInterface<T, V>, Remote {
+public interface ClientToBrokerInterface<T, V> extends Remote {
 
     /**
      * Periodically called by Broker to make sure client is online
-     * @param pingMessage the specific message to send to the client
-     * @return the client's response to the ping
+     * @param userUpdateInfo information of all the users and their active status
      * @throws RemoteException
      */
-    V sendPing(T pingMessage) throws RemoteException;
+    void sendUserUpdateInfo(List<T> userUpdateInfo) throws RemoteException;
 }
