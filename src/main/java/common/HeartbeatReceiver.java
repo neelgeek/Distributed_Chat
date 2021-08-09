9d4815ddbad638704bf32c331a726683b3afec79
Neel Bhave <neelbhave91@gmail.com>
2021-08-09 01:13:20 -0400

    Added comments for new interfaces and impl

--- src/main/java/common/HeartbeatReceiver.java
+++ src/main/java/common/HeartbeatReceiver.java
@@ -3,8 +3,20 @@ package common;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
+/**
+ * Defines an interface for the classes that act as a server that receives heartbeats from its
+ * client instances
+ *
+ * @param <I> Type in which the client info is conveyed.
+ */
 public interface HeartbeatReceiver<I> extends Remote {
 
+  /**
+   * Should be called by the client instance as a heartbeat call.
+   *
+   * @param clientInfo payload containing the information about the client
+   * @throws RemoteException
+   */
   void sendHeartBeat(I clientInfo) throws RemoteException;
 
 }
