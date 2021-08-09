aefaece104175daa73697851fe9af41dfc3f33d8
kulkarniakash <kulkarni.akash@gmail.com>
2021-08-09 16:49:20 -0400

    added discoverBroker method

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -1,5 +1,52 @@
 package client;
 
-public class Client {
+import admin.AdminInterface;
+import common.OutputHandler;
+import common.RMIHandler;
+import protocol.BrokerInfoPayload;
+import protocol.UserInfoPayload;
+
+import java.rmi.RemoteException;
+import java.util.List;
+
+/**
+ * class represents client
+ */
+public class Client implements ClientToBrokerInterface {
+
+    private String rmiName;
+    private String username;
+    private BrokerInfoPayload bip = null;
+
+    public Client(String rmiName, String username) {
+        this.rmiName = rmiName;
+        this.username = username;
+    }
+
+    /**
+     * Requests the admin to assign a broker to this client
+     * @param adminName RMI name of the admin
+     * @param host hostname of the rmi registry
+     * @throws RemoteException
+     * @throws InterruptedException
+     */
+    public void discoverBroker(String adminName, String host) throws RemoteException, InterruptedException {
+
+        try {
+            AdminInterface admin = RMIHandler.fetchRemoteObject(adminName, host);
+            UserInfoPayload uip = new UserInfoPayload(rmiName, username, true);
+            bip = admin.registerNewUser(uip);
+        } catch (NullPointerException e) {
+            OutputHandler.printWithTimestamp("Error: admin object is null");
+        } catch (RemoteException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void sendUserUpdateInfo(List userUpdateInfo) throws RemoteException {
+
+    }
     // entid
 }
--- src/main/java/client/StartClient.java
+++ src/main/java/client/StartClient.java
@@ -1,8 +1,34 @@
 package client;
 
+import common.OutputHandler;
+import common.RMIHandler;
+
+import java.rmi.RemoteException;
+
 public class StartClient {
 
-    public static void main(String args[]) {
+    /**
+     * starts the client and asks for a broker from the admin
+     * @param args arguments of the form "username PORT"
+     * @throws RemoteException
+     * @throws InterruptedException
+     */
+    public static void main(String args[]) throws RemoteException, InterruptedException {
+        // change later to make the name unique
+        String rmiName = "client";
+        String adminName = "admin";
 
+        String username = args[0];
+        Integer PORT = Integer.valueOf(args[1]);
+        String successMessage = "Client " + rmiName + " successfully registered";
+        Client client = new Client(rmiName, username);
+
+        try {
+            RMIHandler.registerRemoteObject(rmiName, client, PORT, successMessage);
+            client.discoverBroker(adminName, null);
+        } catch (RemoteException | InterruptedException e) {
+            OutputHandler.printWithTimestamp("Error: broker discovery failed");
+            e.printStackTrace();
+        }
     }
 }
